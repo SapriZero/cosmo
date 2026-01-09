e372b23825108e521d4e5c5f9e8ee55adf19f585
SapriZero <sapriqbit@gmail.com>
2026-01-09 12:49:02 +0100

    Update UIUtils.js

--- docs/js/ui/UIUtils.js
+++ docs/js/ui/UIUtils.js
@@ -2,8 +2,9 @@
 window.UIUtils = {
 
     getBodyRadius(mass) {
-        const radius = 0.03;  // + 0.08 * Math.pow(mass, 1/3);
-        return Math.max(0.03, radius);
+     //   const radius = 0.03;  // + 0.08 * Math.pow(mass, 1/3);
+       // return Math.max(0.03, radius);
+         return 0.2; // fisso e grande per test
     },
     generateColors(n) {
         const colors = [];
